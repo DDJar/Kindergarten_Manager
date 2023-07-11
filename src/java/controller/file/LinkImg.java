621ae855e72c0531d7a20ee7d48bbed9265b21fc
DDJar <syquocvietoppo@gmail.com>
2023-07-11 18:30:53 +0700

    Updating New

---
+++ src/java/controller/file/LinkImg.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.file;
+
+/**
+ *
+ * @author PC
+ */
+public class LinkImg {
+    public static String pathImg(){
+        return "C:\\Users\\ASUS-PRO\\OneDrive\\Documents\\NetBeansProjects\\SWP391\\A01_RT03\\web\\img\\";
+    }
+}
